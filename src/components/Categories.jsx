3229bde3fa6e94a73bf12be1fc6d1cc5bd798d73
Nazar Yarmolchik <yrnazarusa@gmail.com>
2022-09-10 19:02:14 +0300

    Подключение гита к проекту

---
+++ src/components/Categories.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0)
+
+  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"]
+
+  const onClickCategories = (index) => {
+    setActiveIndex(index)
+  }
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li onClick={() => onClickCategories(index)}
+              className={activeIndex === index ? "active" : ""}>{value}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+
+export default Categories;
+
